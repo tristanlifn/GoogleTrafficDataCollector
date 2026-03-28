d8883bdeed080e9979f82c0b42edb7cac8b0db06
tristan <mail@mail.com>
2026-03-29 00:59:27 +0100

    added cofig file for greater flexiblety and security

---
+++ GoogleTrafficDataCollector/Config.cs
@@ -0,0 +1,8 @@
+namespace HeadlessBrowserTest;
+
+public class Config
+{
+    public string GoogleApiKey { get; set; }
+    public string OriginAddress { get; set; }
+    public string DestinationAddress { get; set; }
+}
--- GoogleTrafficDataCollector/Program.cs
+++ GoogleTrafficDataCollector/Program.cs
@@ -22,38 +22,44 @@ class Program
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase
     };
 
-    private static readonly HttpClient HttpClient = new();
+    private readonly HttpClient HttpClient = new();
     private const string Url = "https://routes.googleapis.com/directions/v2:computeRoutes";
-    private const string Key = "";
-    private const string DestinationAddress = "GFMC+36 Kolding, Denmark";
-    private const string OriginAddress = "Stenholt 37, 6092 Sønder Stenderup, Denmark";
+    private static string _key = "";
+    private static string _destinationAddress = "";
+    private static string _originAddress = "";
 
-    private const string RequestBody = $$"""
-                                         {
-                                                     "computeAlternativeRoutes": false,
-                                                     "destination": {
-                                                         "address": "{{DestinationAddress}}"
-                                                     },
-                                                     "origin": {
-                                                         "address": "{{OriginAddress}}"
-                                                     },
-                                                     "polylineQuality": "overview",
-                                                     "routeModifiers": {
-                                                         "avoidTolls": false,
-                                                         "avoidHighways": false,
-                                                         "avoidFerries": false,
-                                                         "avoidIndoor": false
-                                                     },
-                                                     "routingPreference": "TRAFFIC_AWARE_OPTIMAL",
-                                                     "travelMode": "DRIVE",
-                                                     "languageCode": "en-US",
-                                                     "units": "METRIC"
-                                                 }
-                                         """;
+    private string _requestBody = $$"""
+        {
+            "computeAlternativeRoutes": false,
+            "destination": {
+                "address": "{{_destinationAddress}}"
+            },
+            "origin": {
+                "address": "{{_originAddress}}"
+            },
+            "polylineQuality": "overview",
+            "routeModifiers": {
+                "avoidTolls": false,
+                "avoidHighways": false,
+                "avoidFerries": false,
+                "avoidIndoor": false
+            },
+            "routingPreference": "TRAFFIC_AWARE_OPTIMAL",
+            "travelMode": "DRIVE",
+            "languageCode": "en-US",
+            "units": "METRIC"
+        }
+        """;
 
     private Program()
     {
-        string documents = nameof(Environment.SpecialFolder.MyDocuments);
+        if (!LoadConfig())
+        {
+            Console.WriteLine("No valid config file found in execution directory.");
+            return;
+        }
+        
+        string documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         string fileName = $"{documents}/routes.json";
 
         try
@@ -63,13 +69,16 @@ class Program
             
             RoutesResponse result = ComputeRoutesAsync().Result;
             
+            if (result.Routes?.Count <= 0)
+                return;
+            
             string oldJson = File.ReadAllText(fileName);
 
             List<Route> oldRoutsList = [];
             if (!string.IsNullOrWhiteSpace(oldJson))
                 oldRoutsList = JsonSerializer.Deserialize<RoutesResponse>(oldJson, JsonOptions)?.Routes ?? [];
             
-            result.Routes.AddRange(oldRoutsList);
+            result.Routes?.AddRange(oldRoutsList);
             
             File.WriteAllText(fileName, JsonSerializer.Serialize(result, JsonOptions));
         }
@@ -79,15 +88,30 @@ class Program
         }
     }
 
-    private static async Task<RoutesResponse> ComputeRoutesAsync()
+    private bool LoadConfig()
+    {
+        string configString = File.ReadAllText("config.json");
+        Config config = JsonSerializer.Deserialize<Config>(configString, JsonOptions) ?? new Config();
+        
+        if (string.IsNullOrEmpty(config.GoogleApiKey) || string.IsNullOrEmpty(config.OriginAddress) || string.IsNullOrEmpty(config.DestinationAddress))
+            return false;
+        
+        _key = config.GoogleApiKey;
+        _originAddress = config.OriginAddress;
+        _destinationAddress = config.DestinationAddress;
+        
+        return true;
+    }
+    
+    private async Task<RoutesResponse> ComputeRoutesAsync()
     {
         string json = "";
         
         try
         {
             using HttpRequestMessage request = new(HttpMethod.Post, Url);
-            request.Content = new StringContent(RequestBody, Encoding.UTF8, "application/json");
-            request.Headers.Add("X-Goog-Api-Key", Key);
+            request.Content = new StringContent(_requestBody, Encoding.UTF8, "application/json");
+            request.Headers.Add("X-Goog-Api-Key", "AIzaSyCrQMbThAV4ZaXdq5-e82RcAJ5EEWmik_0");
             request.Headers.Add("X-Goog-FieldMask",
                 "routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline");
 
